f86c881b8f601db16b4521aa1e194f2534b5869f
Aiiwi <140857029+Aiiwi@users.noreply.github.com>
2025-02-19 09:51:42 +0100

    Update player.gd

--- player.gd
+++ player.gd
@@ -4,10 +4,10 @@ extends CharacterBody3D
 @export_range(0.1,1.0) var mouse_sensitivity :float = 0.2 #Sensibilty !!change this to reference a global var
 
 @export_group("Player_Movement")
-@export var move_speed := 5.0 #Player normal speed
-@export var acceleration := 20.0
+@export var move_speed :float = 5.0 #Player normal speed
+@export var acceleration :float = 20.0
 
-@onready var pause_ui= $CameraPoint/UI/Pause_UI as CanvasLayer
+@onready var pause_ui:CanvasLayer= $CameraPoint/UI/Pause_UI as CanvasLayer
 @onready var _camera:Camera3D = %CameraPoint
 
 var _camera_input_direction := Vector2.ZERO
